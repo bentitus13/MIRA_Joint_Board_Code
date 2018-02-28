4f83df1ccbbef02651f5eeeda1ffc3c9b039921a
Ben Titus <ben.titus13@gmail.com>
2018-02-27 22:12:12 -0500

    Deleted old project, added new project

---
+++ MIRA_Joint_Code/Debug/configPkg/package/cfg/config_pem4f.h
@@ -0,0 +1,25 @@
+/*
+ *  Do not modify this file; it is automatically 
+ *  generated and any modifications will be overwritten.
+ *
+ * @(#) xdc-B24
+ */
+
+#include <xdc/std.h>
+
+#include <ti/sysbios/knl/Clock.h>
+extern const ti_sysbios_knl_Clock_Handle Motor_Clock;
+
+#include <ti/sysbios/knl/Semaphore.h>
+extern const ti_sysbios_knl_Semaphore_Handle Motor_Semaphore;
+
+#include <ti/sysbios/knl/Task.h>
+extern const ti_sysbios_knl_Task_Handle Motor_Task;
+
+#include <ti/sysbios/knl/Clock.h>
+extern const ti_sysbios_knl_Clock_Handle Heartbeat_Clock;
+
+extern int xdc_runtime_Startup__EXECFXN__C;
+
+extern int xdc_runtime_Startup__RESETFXN__C;
+
